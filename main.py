86124fe996d8759712cafb8fb7d23c15346e288b
Jeremy <jeremymdunne@gmail.com>
2018-06-04 20:21:36 -0400

    In Class form

--- main.py
+++ main.py
@@ -1,47 +1,50 @@
 import tkinter as tk
 from tkinter import filedialog
-BURN_TIME_TOLLERENCE = .03
-GRAVITY = 9.81
+
 root = tk.Tk()
 root.withdraw()
 
-engineData = []
-propellantWeight = 0
-burnTime = 0
-totalThrust = 0
-maxThrust = []
-impulse = 0
 
-def getMaxThrust():
-    global maxThrust
-    maxThrust = engineData[0]
-    for i in engineData:
-        if i[0] > maxThrust[0]:
-            maxThrust = i
 
-def getPropellantWeight():
-    global propellantWeight
-    propellantWeight = abs(engineData[0][0] - engineData[-1][0])
+class WeightToThrustAnalyzer():
+    BURN_TIME_TOLLERENCE = .03
+    GRAVITY = 9.81
+    engineData = []
 
-def getBurnTime():
-    global burnTime
-    for i in engineData:
-        if abs(1 - (i[0]/engineData[-1][0])) <= BURN_TIME_TOLLERENCE:
-            burnTime = i[1] - engineData[0][1]
-            return
+    def __init__(self, weightData):
+        self.engineData = weightData
+        self.convertToThrust()
 
-def convertToThrust():
-    for i in engineData:
-        i[0] *= GRAVITY
+    def getMaxThrust(self):
+        maxThrust = self.engineData[0]
+        for i in self.engineData:
+            if i[0] > maxThrust[0]:
+                maxThrust = i
+        return maxThrust
 
-def getImpulse():
-    area = 0
-    for i in range(0,len(engineData)-1):
-        midpoint = (engineData[i + 1][0] - engineData[i][0])/2.0 + engineData[i][0]
-        tempArea = midpoint * (engineData[i+1][1] - engineData[i][1])/1000.0
-        if(tempArea > 0):
-            area += tempArea
-    return area
+    def getPropellantWeight(self):
+        return abs(self.engineData[0][0] - self.engineData[-1][0])/self.GRAVITY
+
+    def getBurnTime(self):
+        for i in self.engineData:
+            if abs(1 - (i[0]/self.engineData[-1][0])) <= self.BURN_TIME_TOLLERENCE:
+                burnTime = i[1] - self.engineData[0][1]
+                return burnTime
+
+    def convertToThrust(self):
+        for i in self.engineData:
+            i[0] *= self.GRAVITY
+
+    def getImpulse(self):
+        area = 0
+        for i in range(0,len(self.engineData)-1):
+            midpoint = (self.engineData[i + 1][0] - self.engineData[i][0])/2.0 + self.engineData[i][0]
+            tempArea = midpoint * (self.engineData[i+1][1] - self.engineData[i][1])/1000.0
+            if(tempArea > 0):
+                area += tempArea
+        return area
+
+weightData = []
 
 def parseString(line):
     weightIndex = line.index("@{")
@@ -49,19 +52,16 @@ def parseString(line):
     endLine = line.index("}@")
     weight = line[(weightIndex + 2):timeIndex]
     time = line[(timeIndex + 1):endLine-1]
-    engineData.append([float(weight), float(time)])
+    return [float(weight), float(time)]
 
 def main():
     file = open(filedialog.askopenfilename(), 'r')
     for line in file:
-        parseString(line)
-    getPropellantWeight();
-    print("Propellant Weight: " + str(propellantWeight))
-    convertToThrust()
-    getMaxThrust()
-    print("Max Thrust: " + str(maxThrust[0]) + " Newtons At Time: " + str(maxThrust[1]))
-    getBurnTime();
-    print("Burn time: " + str(burnTime))
-    print("Total Impulse: " + str(getImpulse()))
+        weightData.append(parseString(line))
+    thrustData = WeightToThrustAnalyzer(weightData)
+    print("Propellant Weight: " + str(thrustData.getPropellantWeight()))
+    print("Max Thrust: " + str(thrustData.getMaxThrust()[0]) + " Newtons At Time: " + str(thrustData.getMaxThrust()[1]))
+    print("Burn time: " + str(thrustData.getBurnTime()))
+    print("Total Impulse: " + str(thrustData.getImpulse()))
 
 main()
